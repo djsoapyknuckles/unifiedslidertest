ef4cdaae2b4c2689c1849b41906e4f7cb4b91d6c
djsoapyknuckles <djsoapyknuckles@gmail.com>
2020-12-20 11:10:19 -0500

    initial commit

--- DildoMachine2UnifiedScaleSlider.cs
+++ DildoMachine2UnifiedScaleSlider.cs
@@ -1,52 +1,410 @@
+using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using System.Linq;
+using System.IO;
 
 public class DildoMachine2UnifiedScaleSlider : MVRScript
 {
+    // My JSONStorables
+    public JSONStorableBool animOn_Off;
+    public JSONStorableFloat SCALEsliderFloat;
+    public JSONStorableFloat SCALEdefaultdildo;
+    public JSONStorableFloat SCALEchanceunflareddildo;
+    public JSONStorableFloat SCALErexdildo;
+    public JSONStorableFloat SCALEtuckerdildo;
+    public JSONStorableFloat ROTATIONsliderFloat;
+    public JSONStorableFloat ANIMSPEEDsliderFloat;
+    public JSONStorableStringChooser dildoChoice;
+
+    // My misc variables to find and edit the prefab made in Unity
+    private string objectNameSearchString = "dildoMachine";
+    public Transform ObjectRoot;
+    private Transform machineTR;
+
+    private Transform defaultDildo;
+    private Transform chanceUnflaredDildo;
+    private Transform rexDildo;
+    private Transform tuckerDildo;
+    private Transform[] dildoArray;
+    private Animator _animator;
+    private Collider[] colliderArray;
+    private Transform[] children;
+    private Vector3[] _connectedAnchor;
+    private Vector3[] _anchor;
+    private Vector3 newScale;
+
+    //UI variables
+    protected UIDynamicSlider SCALEslider;
+    protected UIDynamicSlider ROTATIONslider;
+    protected UIDynamicPopup CHOOSERudp;
+
+    // Security and initialization
+    private bool editorInitialized = false;
+    private int initLoops = 0;
+    private int maxInitLoops = 300;
+    private Transform selectedDildo = null;
     public override void Init()
     {
         try
         {
-            SuperController.LogMessage($"{nameof(MyPlugin)} initialized");
+            // We only authorize the script to be used by a CUA
+            if (containingAtom.type != "CustomUnityAsset")
+            {
+                //**************UI CODE GOES HERE*********************
+                //animation toggle
+                animOn_Off = new JSONStorableBool("animation on/off", false, animOn_OffCallback);
+                animOn_Off.storeType = JSONStorableParam.StoreType.Full;
+                CreateToggle(animOn_Off, false);
+                RegisterBool(animOn_Off);
+
+                /* // JSONStorableFloat Rotation Slider
+                 ROTATIONsliderFloat = new JSONStorableFloat("Rotation", 0f, RotationFloatCallback, 0f, 90f, true);
+                 ROTATIONsliderFloat.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(ROTATIONsliderFloat);
+                 ROTATIONslider = CreateSlider(ROTATIONsliderFloat, false);
+
+                 // JSONStorableFloat Animation Speed Slider
+                 ANIMSPEEDsliderFloat = new JSONStorableFloat("Animation Speed", 1f, AnimSpeedFloatCallback, 0.5f, 5f, true);
+                 ANIMSPEEDsliderFloat.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(ANIMSPEEDsliderFloat);
+                 ROTATIONslider = CreateSlider(ANIMSPEEDsliderFloat, false);
+
+                 //JSONStorableChooser
+                 List<string> choices = new List<string>();
+                 //choices.Add("None");
+                 choices.Add("Default");
+                 choices.Add("Chance Unflared");
+                 choices.Add("Rex");
+                 choices.Add("Tucker");
+                 //choices.Add("Choice3");
+                 dildoChoice = new JSONStorableStringChooser("Chooser", choices, "Default", "Choose Dildo", DildoChooserCallback);
+                 dildoChoice.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterStringChooser(dildoChoice);
+                 UIDynamicPopup udp = CreatePopup(dildoChoice, false);
+                 //CreateScrollablePopup(jchooser, true);
+                 udp.labelWidth = 100f;
+
+                 //Register SCALE StorableFloats for each dildo
+                 SCALEdefaultdildo.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(SCALEdefaultdildo);
+                 SCALEchanceunflareddildo.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(SCALEchanceunflareddildo);
+                 SCALErexdildo.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(SCALErexdildo);
+                 SCALEtuckerdildo.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(SCALEtuckerdildo);
+
+                 //JSONStorableFloat Scale Slider
+                 SCALEsliderFloat = new JSONStorableFloat("Scale Selected Dildo", 1f, ScaleFloatCallback, 0.5f, 1.5f, true);
+                 SCALEsliderFloat.storeType = JSONStorableParam.StoreType.Full;
+                 RegisterFloat(SCALEsliderFloat);
+                 SCALEslider = CreateSlider(SCALEsliderFloat, false);*/
+
+            }
         }
         catch (Exception e)
         {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(Init)}: {e}");
+            SuperController.LogError("Dildo Machine Controller: Exception caught: " + e);
         }
     }
 
-    public void OnEnable()
+    // Used for the initialization of the editor.
+    // When it is initialized, there's nothing done every frame
+    void Update()
     {
-        try
+        // If the editor is not initialized, we're gonna try to do it
+        // I'm putting 3600 tries ( at 60fps it is 1min )
+        if (initLoops < maxInitLoops && editorInitialized == false)
         {
-            SuperController.LogMessage($"{nameof(MyPlugin)} enabled");
+            initLoops++;
+            InitEditor();
         }
-        catch (Exception e)
+
+        // Triggering an error if we have reached our limit of tries
+        if (initLoops == maxInitLoops)
         {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnEnable)}: {e}");
+            SuperController.LogError("Dildo Machine Controller: This CUA could not be initialized properly after 3600 tries. Please select a compatible object and reload the script.");
+            initLoops++; // To avoid spamming the log with the error
         }
     }
 
-    public void OnDisable()
+    private bool InitEditor()
     {
-        try
+
+        if (editorInitialized == true) return true;
+
+
+        ObjectRoot = getObjectRoot(containingAtom.reParentObject);
+        if (ObjectRoot == null)
         {
-            SuperController.LogMessage($"{nameof(MyPlugin)} disabled");
+            return false;
         }
-        catch (Exception e)
+
+        // Find first
+        machineTR = ObjectRoot.Find("mechanical");
+
+        defaultDildo = ObjectRoot.Find("dildo");
+
+        chanceUnflaredDildo = ObjectRoot.Find("chanceUnflared_37k_rigged");
+
+        rexDildo = ObjectRoot.Find("rex_10k_rigged");
+
+        tuckerDildo = ObjectRoot.Find("tucker_23k_rigged");
+
+        dildoArray = new Transform[] { defaultDildo, chanceUnflaredDildo, rexDildo, tuckerDildo };
+
+        // found my transforms, now the rest
+        if (machineTR && defaultDildo && chanceUnflaredDildo && rexDildo && tuckerDildo)
         {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnDisable)}: {e}");
+            // Finding Animator component
+            _animator = machineTR.GetComponentInChildren<Animator>();
+
+
+            // Found Animator...
+            if (_animator)
+            {
+                // Initialized
+                editorInitialized = true;
+
+                // Initialize script based on the information in my JSONStorables
+
+                //these are the values stored on save
+                //animOn_OffCallback(animOn_Off.val);
+                // SuperController.LogMessage("anim_on_OffCallback called from update InitEditor");
+                //RotationFloatCallback(ROTATIONsliderFloat);
+                // SuperController.LogMessage("RotationFloatCallback called from update InitEditor");
+                // AnimSpeedFloatCallback(ANIMSPEEDsliderFloat);
+                // SuperController.LogMessage("AnimSpeedFloatCallback called from update InitEditor");
+                //ScaleFloatCallback(SCALEsliderFloat);
+                // SuperController.LogMessage("ScaleFloatCallback called from update InitEditor with value" + jfloat3.val);
+                // DildoChooserCallback(dildoChoice.val);
+
+
+
+                SuperController.LogMessage("Dildo Machine Controller: script initialized properly");
+                return true;
+            }
+            else
+            {
+                SuperController.LogError("Dildo Machine Controller : transform found, but does not have Animator attached.");
+                initLoops = maxInitLoops; // We don't have what we need, we consider the initialization failed
+                return false;
+            }
+        }
+        else
+        {
+            SuperController.LogError("Dildo Machine Controller : missing mandatory transform object");
+            initLoops = maxInitLoops; // We don't have what we need, we consider the initialization failed
+            return false;
         }
     }
 
-    public void OnDestroy()
+
+    /*
+     * *************Callbacks*********************************
+    */
+    protected void animOn_OffCallback(bool on_off)
     {
-        try
+
+        if (machineTR)
         {
-            SuperController.LogMessage($"{nameof(MyPlugin)} destroyed");
+            if (on_off)
+            {
+                _animator.enabled = true;
+            }
+            else
+            {
+                _animator.enabled = false;
+            }
+
         }
-        catch (Exception e)
+
+    }
+
+    protected void RotationFloatCallback(JSONStorableFloat rotation)
+    {
+        if (machineTR)
         {
-            SuperController.LogError($"{nameof(MyPlugin)}.{nameof(OnDestroy)}: {e}");
+            //SuperController.LogMessage("Float param " + jf.name + " set to " + jf.val);
+            machineTR.transform.localEulerAngles = new Vector3(rotation.val, 0.0f, 0.0f);
+        }
+    }
+    protected void AnimSpeedFloatCallback(JSONStorableFloat animSpeed)
+    {
+        if (machineTR)
+        {
+
+            //SuperController.LogMessage("Float param " + animSpeed.name + " set to " + animSpeed.val);
+            _animator.speed = animSpeed.val;
+        }
+    }
+
+
+    protected void DildoChooserCallback(string choice)
+    {
+
+        if (choice == "Default")
+        { selectedDildo = defaultDildo; }
+        else
+        if (choice == "Chance Unflared")
+        { selectedDildo = chanceUnflaredDildo; }
+        else
+        if (choice == "Rex")
+        { selectedDildo = rexDildo; }
+        else
+        if (choice == "Tucker")
+        { selectedDildo = tuckerDildo; }
+
+
+        for (int i = 0; i < dildoArray.Length; i++)
+        {
+            if (selectedDildo != null)
+            {
+                if (dildoArray[i] == selectedDildo)
+                {
+                    EnableMesh(selectedDildo);
+                    DoTurnCollidersforChildrenON(selectedDildo);
+                }
+                else
+                {
+                    DisableMesh(dildoArray[i]);
+                    DoTurnCollidersforChildrenOFF(dildoArray[i]);
+                }
+            }
+        }
+    }
+    protected void ScaleFloatCallback(JSONStorableFloat scale)
+    {
+        if (selectedDildo == defaultDildo)
+        {
+            SCALEdefaultdildo = scale;
+        }
+        else if
+            (selectedDildo == chanceUnflaredDildo)
+        {
+            SCALEchanceunflareddildo = scale;
+        }
+        else if
+            (selectedDildo == rexDildo)
+        {
+            SCALErexdildo = scale;
+        }
+        else if
+            (selectedDildo == tuckerDildo)
+        {
+            SCALEtuckerdildo = scale;
+        }
+
+        for (int i = 0; i < dildoArray.Length; i++)
+        {
+            if (selectedDildo != null)
+            {
+                if (dildoArray[i] == selectedDildo)
+                {
+                    DoGetConnectedAnchorandAnchorVals(selectedDildo);
+                    DoTurnCollidersforChildrenOFF(selectedDildo);
+                    DoScaleTransform(selectedDildo, scale.val);
+                    DoApplyConnectedAnchorandAnchorVals(selectedDildo);
+                    DoTurnCollidersforChildrenON(selectedDildo);
+                }
+            }
+        }
+    }
+
+    /*
+    *********************UTILITY METHODS**********************
+    */
+
+    private Transform getObjectRoot(Transform parent)
+    // Recursive function searching for a transform containing the string set in objectNameSearchString
+    // It is used the first time for the initialization of the script
+    {
+        foreach (Transform child in parent)
+        {
+            if (child.name.Contains(objectNameSearchString))
+            {
+                return child;
+            }
+            else
+            {
+                Transform childSearch = getObjectRoot(child);
+                if (childSearch != null) return childSearch;
+            }
+        }
+
+        return null;
+    }
+
+    protected void DoScaleTransform(Transform t, float s)
+    {
+        newScale = new Vector3(s, s, s);
+        t.transform.localScale = newScale;
+    }
+
+    protected void DoGetConnectedAnchorandAnchorVals(Transform t)
+    {
+        children = t.transform.GetComponentsInChildren<Transform>();
+        _connectedAnchor = new Vector3[children.Length];
+        _anchor = new Vector3[children.Length];
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (children[i].GetComponent<CharacterJoint>() != null)
+            {
+                _connectedAnchor[i] = children[i].GetComponent<CharacterJoint>().connectedAnchor;
+                _anchor[i] = children[i].GetComponent<CharacterJoint>().anchor;
+            }
+        }
+    }
+    protected void DoApplyConnectedAnchorandAnchorVals(Transform t)
+    {
+        children = t.transform.GetComponentsInChildren<Transform>();
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (children[i].GetComponent<CharacterJoint>() != null)
+            {
+                children[i].GetComponent<CharacterJoint>().connectedAnchor = _connectedAnchor[i];
+                children[i].GetComponent<CharacterJoint>().anchor = _anchor[i];
+            }
+        }
+    }
+    protected void EnableMesh(Transform t)
+    {
+        Renderer Mesh = t.GetComponentInChildren<Renderer>();
+        Mesh.enabled = true;
+    }
+
+    protected void DisableMesh(Transform t)
+    {
+        Renderer Mesh = t.GetComponentInChildren<Renderer>();
+        Mesh.enabled = false;
+    }
+    protected void DoTurnCollidersforChildrenON(Transform t)
+    {
+        colliderArray = t.GetComponentsInChildren<Collider>();
+        for (int i = 0; i < colliderArray.Length; i++)
+        {
+            if (colliderArray[i].GetComponent<Collider>() != null)
+            {
+                colliderArray[i].GetComponent<Collider>().enabled = true;
+            }
+        }
+    }
+
+    protected void DoTurnCollidersforChildrenOFF(Transform t)
+    {
+        colliderArray = t.GetComponentsInChildren<Collider>();
+        for (int i = 0; i < colliderArray.Length; i++)
+        {
+            if (colliderArray[i].GetComponent<Collider>() != null)
+            {
+                colliderArray[i].GetComponent<Collider>().enabled = false;
+            }
         }
     }
 }
+
+
+
